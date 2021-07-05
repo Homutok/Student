5a9c7f71fe63592d310b636310a805ac208045ba
Homutok <rsanya2000@gmail.com>
2021-07-05 19:05:02 +0300

    Model_v1 Созданы модели, надо доработать модел Profile

--- project/studenlist/catalog/admin.py
+++ project/studenlist/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Profile,Comment,Department
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Department)
--- project/studenlist/catalog/models.py
+++ project/studenlist/catalog/models.py
@@ -1,10 +1,73 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime
 
+
+#Модель студента
 class Student(models.Model):
-    pass
+    student_name = models.CharField(max_length=100,db_index=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    place_of_study = models.CharField(max_length=200,db_index=True)
+    faculty = models.CharField(max_length=200,db_index=True)
+
+    LOAN_STATUS = (
+        ('s', 'Учится'),
+        ('n', 'Отчислен'),
+        ('w', 'Работает'),
+        ('nw', 'Уволен'),
+    )
+    status = models.CharField(max_length=2, choices=LOAN_STATUS, blank=True, default='s', help_text='статус ученика')
+    summary = models.CharField(max_length=1000,db_index=True)
+
+    mentor_id = models.ForeignKey('Profile',on_delete=models.PROTECT,null=True)
+    department_id = models.ForeignKey('Department',on_delete=models.PROTECT,null=True)
+
+    begin_of_study = models.DateField(null=True, blank=True)
+    @property
+    def calculate_course(self):
+        if self.begin_of_study:
+            today = datetime.date.today()
+            return today.year - self.begin_of_study.year - (
+                        (today.month, today.day) < (self.begin_of_study.month, self.begin_of_study.day))
+        return 0
+
+    def __str__(self):
+        return self.student_name
+
+
+#Модель комментариев
 class Comment(models.Model):
-    pass
+    comment = models.CharField(max_length=200,db_index=True)
+
+    mentor_id = models.ForeignKey('Profile',on_delete=models.PROTECT,null=True)
+    student_id = models.ForeignKey('Student',on_delete=models.PROTECT,null=True)
+
+    def __str__(self):
+        return self.comment
+
+#Расширение стандартного пользователя
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role_of_user = models.CharField(max_length=100,db_index=True)
+
+    department_id = models.ForeignKey('Department',on_delete=models.PROTECT,null=True)
+
+    def __str__(self):
+        return self.role_of_user
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+#Класс отдела
 class Department(models.Model):
-    pass
+    department_name=models.CharField(max_length=100,db_index=True)
+
+    def __str__(self):
+        return self.department_name

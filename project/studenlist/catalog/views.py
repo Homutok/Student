1dd8e8a2943f9c9262dbc27d533d45276ba57b75
Ev <kresch2019@gmail.com>
2021-07-06 14:44:58 +0300

    не успел изменить ((

--- project/studenlist/catalog/views.py
+++ project/studenlist/catalog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Student, Profile, Comment, Department,University
 from django.views import generic
 from django.http import Http404
-
+from .forms import FilterStudentForm
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -21,6 +22,12 @@ class StudentListView(generic.ListView):
     context_object_name = 'student_list'
     template_name = 'catalog/student_list.html'
 
+    #def get_queryset(self): # новый
+    #    query = self.request.GET.get('q')
+    #    student_list = Student.objects.filter(
+    #        Q(student_name__icontains=query) | Q(status__icontains=query)
+    #    )
+    #    return student_list
 
 class StudentDetailView(generic.DetailView):
     model = Student
@@ -29,8 +36,6 @@ class StudentDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comment_data'] = Comment.objects.filter(comment=self.object)
-
-        print(context)
         return context
 
     def guide_detail_view(request, pk):
@@ -75,7 +80,16 @@ class DepartmentDetailView(generic.DetailView):
     template_name = 'catalog/department_detail.html'
     paginate_by = 10
 
-    #def get_context_data(self, **kwargs):
-    #    context = super().get_context_data(**kwargs)
-    #    context['borrowed'] = True
-    #    return context
+
+def filter_students(request):
+    if request.method == "POST":
+        form = FilterStudentForm(request.POST)
+        if form.is_valid():  # All validation rules pass
+            student_name = form.cleaned_data['student_name']
+            faculty = form.cleaned_data['faculty']
+            status = form.cleaned_data['status']
+
+            return render(request, 'catalog/student_list.html', {'form': form})
+    else:
+        form = FilterStudentForm()
+    return render(request, 'catalog/student_list.html', {'form': form})

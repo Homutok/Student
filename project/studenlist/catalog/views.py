e73b35398dfdbd5d62eee4c6dc3deeb05c2e552b
Homutok <rsanya2000@gmail.com>
2021-07-06 10:43:44 +0300

    Создана примерная карта сайта 1.0

--- project/studenlist/catalog/views.py
+++ project/studenlist/catalog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Student, Profile,Comment,Department
+from .models import Student, Profile, Comment, Department
+from django.views import generic
+
+
 # Create your views here.
 def index(request):
     num_working_student = Student.objects.filter(status__exact='study').count()
@@ -10,4 +13,29 @@ def index(request):
                            'num_department': num_department,
                            'num_mentors': num_mentors})
 
-#class StudentListView()
+
+# Просмотр страницы с студентами
+class StudentListView(generic.ListView):
+    pass
+
+
+class StudentDetailView(generic.DetailView):
+    pass
+
+
+# Просмотр списка университетов
+class UniversityListView(generic.ListView):
+    pass
+
+
+class UniversityDetailView(generic.DetailView):
+    pass
+
+
+# Просмотр отделов
+class DepartmentListView(generic.ListView):
+    pass
+
+
+class DepartmentDetailView(generic.DetailView):
+    pass
